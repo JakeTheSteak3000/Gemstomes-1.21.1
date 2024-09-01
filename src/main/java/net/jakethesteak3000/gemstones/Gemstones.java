665b8d40464794563e91fde238bb23061db57fd5
Jake Davidson <jakeodavidson1@gmail.com>
2024-09-01 16:02:38 -0400

    Added Gemstones

--- src/main/java/net/jakethesteak3000/gemstones/Gemstones.java
+++ src/main/java/net/jakethesteak3000/gemstones/Gemstones.java
@@ -1,6 +1,8 @@
 package net.jakethesteak3000.gemstones;
 
 import com.mojang.logging.LogUtils;
+import net.jakethesteak3000.gemstones.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -28,7 +30,7 @@ public class Gemstones {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-
+        ModItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -42,7 +44,12 @@ public class Gemstones {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RUBY);
+            event.accept(ModItems.JADE);
+            event.accept(ModItems.TOPAZ);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/jakethesteak3000/gemstones/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.jakethesteak3000.gemstones.item;
+
+import net.jakethesteak3000.gemstones.Gemstones;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Gemstones.MOD_ID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> JADE = ITEMS.register("jade",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> TOPAZ = ITEMS.register("topaz",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
